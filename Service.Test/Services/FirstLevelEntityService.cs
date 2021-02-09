b55a86a320ca6b1bfa4f6000501cfa66e0589e88
HernanFAR <h.f.alvarez.r@gmail.com>
2021-02-09 04:07:13 -0300

    Se agregaron los tese de FirstLevelEntity y de SecondLevelEntity

---
+++ Service.Test/Services/FirstLevelEntityService.cs
@@ -0,0 +1,15 @@
+﻿using Quicker.Abstracts.Service;
+using Service.Test.Models;
+using Service.Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Test.Services
+{
+    public class FirstLevelEntityService : BaseService<int, FirstLevelEntity>
+    {
+        public FirstLevelEntityService(TestContext context) : 
+            base(context) { }
+    }
+}
---
+++ Service.Test/Services/SecondLevelEntityService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Quicker.Abstracts.Service;
+using Service.Test.Models;
+using Service.Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Test.Services
+{
+    public class SecondLevelEntityService : BaseService<int, SecondLevelEntity, SecondLevelEntityDTO>
+    {
+        public SecondLevelEntityService(TestContext context, IMapper mapper) : 
+            base(context, mapper) { }
+    }
+}
