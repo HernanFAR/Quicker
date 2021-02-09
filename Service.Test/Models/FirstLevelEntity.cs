9f8c3a8214c07645567db257cc70be9c964623f3
HernanFAR <h.f.alvarez.r@gmail.com>
2021-02-09 04:01:21 -0300

    Se agregaron los modelos de FirstLevel y SecondLevel entities, el segundo tiene un DTO

---
+++ Service.Test/Models/FirstLevelEntity.cs
@@ -0,0 +1,16 @@
+﻿using Quicker.Abstracts.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Service.Test.Models
+{
+    public class FirstLevelEntity : AbstractModel<int>
+    {
+        [Required, StringLength(25)]
+        public string Name { get; set; }
+
+        public ICollection<SecondLevelEntity> SecondLevelEntityNavigations { get; set; }
+    }
+}
---
+++ Service.Test/Models/SecondLevelEntity.cs
@@ -0,0 +1,19 @@
+﻿using Quicker.Abstracts.Model;
+using Quicker.Interfaces.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Service.Test.Models
+{
+    public class SecondLevelEntity : AbstractModel<int>, IDomainOf<SecondLevelEntityDTO>
+    {
+        [Required, StringLength(25)]
+        public string Name { get; set; }
+
+        public int FirstLevelEntityId { get; set; }
+
+        public FirstLevelEntity FirstLevelEntityNavigation { get; set; }
+    }
+}
