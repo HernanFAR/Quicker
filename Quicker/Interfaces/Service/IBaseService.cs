8212eb71746006dcd55f736c790e9af1793cf762
HernanFAR <h.f.alvarez.r@gmail.com>
2021-02-09 01:26:51 -0300

    se agrego el paquete automapper

---
+++ Quicker/Interfaces/Service/IBaseService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicker.Interfaces.Service
+{
+    public interface IBaseService<TKey, TEntity>
+    {
+        Task<IQueryable<TEntity>> QueryAll();
+
+        Task<TEntity> QuerySingle(TKey key);
+    }
+
+    public interface IBaseService<TKey, TEntity, TEntityDTO>
+    {
+        Task<IQueryable<TEntity>> QueryAll();
+
+        Task<TEntity> QuerySingle(TKey key);
+
+        TEntityDTO ToDTO(IMapper mapper, TEntity entity);
+
+        TEntity ToDomain(IMapper mapper, TEntityDTO entity);
+    }
+}
