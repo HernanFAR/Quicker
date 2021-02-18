374316b0c8d240898e654eb1a5cd7c133f4407a7
Hernan Alvarez <halvarez@Austranet.com>
2021-02-18 09:24:31 -0300

    Se mejoraron los resumenes de las funciones de BaseService y IBaseService

--- Quicker/Abstracts/Service/BaseService.cs
+++ Quicker/Abstracts/Service/BaseService.cs
@@ -11,34 +11,71 @@ using System.Threading.Tasks;
 namespace Quicker.Abstracts.Service
 {
     /// <summary>
-    /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity}"/>.</para>
+    ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity}"/>, this class 
+    ///     serves the main functions to get single or many elements in database.
     /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         Typically, this class is not inherented, unless you want to create a full
+    ///         implementation of CloseService and OpenService for some reason
+    ///     </para>
+    ///     <para>
+    ///         The utility of this class is centralice all object-retriving code that get objects of DB in two functions.    
+    ///     </para>
+    /// </remarks>
+    /// 
+
     public abstract class BaseService<TKey, TEntity> : IBaseService<TKey, TEntity>
         where TEntity : class, IAbstractModel<TKey>
     {
-        /// <summary>
-        /// <para>Database context of the service.</para>
-        /// </summary>
         protected DbContext Context { get; set; }
 
+        /// <summary>
+        ///     Gives a way to set the <paramref name="context"/> property from subclasses.
+        /// </summary>
+        /// <param name="context">
+        ///     The <see cref="DbContext"/> to be consumed by the service
+        /// </param>
+        /// 
         public BaseService(DbContext context)
         {
             Context = context;
         }
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity}.QueryAll"/>.</para>
-        /// <para>Return a <em>"AsNoTracking"</em> list, to avoid problems with EF Core</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity}.QueryAll"/>.
         /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The utility of this is centralice <em>all</em> object-retriving code that get ALL objects in DB, in one 
+        ///         function (Not counting specific cases), acting like a <em>generic</em>.
+        ///     </para>
+        /// </remarks>
+        /// <returns>
+        ///     A readonly (Not tracked by the <see cref="DbContext"/>) <see cref="IQueryable{TEntity}"/>.
+        /// </returns>
+        /// 
         public IQueryable<TEntity> QueryAll()
             => Context
                 .Set<TEntity>()
                 .AsNoTracking();
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity}.QuerySingle(TKey)"/>.</para>
-        /// <para>Return a entity tracked by the context to fulfill with related entities.</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity}.QuerySingle(TKey)"/>.
         /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The utility of this is centralice <em>all</em> object-retriving code that get ONE object 
+        ///         in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.
+        ///     </para>
+        /// </remarks>
+        /// <returns>
+        ///     Return a <see cref="TEntity"/> founded by <paramref name="key"/> tracked by the context or null.
+        /// </returns>
+        /// <param name="key">
+        ///     The primary key to found the entity
+        /// </param>
+        /// 
         public async Task<TEntity> QuerySingle(TKey key)
             => await Context
                 .Set<TEntity>()
@@ -47,42 +84,96 @@ namespace Quicker.Abstracts.Service
     }
 
     /// <summary>
-    /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity}"/>.</para>
+    ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity TEntityDTO}"/>, this class 
+    ///     serves the main functions to get single or many elements in database.
     /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         Typically, this class is not inherented, unless you want to create a full
+    ///         implementation of CloseService and OpenService for some reason
+    ///     </para>
+    ///     <para>
+    ///         The utility of this class is centralice all object-retriving code that get objects of DB in two functions.    
+    ///     </para>
+    /// </remarks>
+    /// 
     public abstract class BaseService<TKey, TEntity,TEntityDTO> : IBaseService<TKey, TEntity, TEntityDTO>
         where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
         where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
     {
-        /// <summary>
-        /// <para>Database context of the service.</para>
-        /// </summary>
         protected DbContext Context { get; }
 
-        /// <summary>
-        /// <para>Mapper property to map the DTO to Domain, a viceversa.</para>
-        /// </summary>
         protected IMapper Mapper { get;  }
 
+        /// <summary>
+        ///     Gives a way to set the <paramref name="context"/> and <paramref name="mapper"/> properties from subclasses.
+        /// </summary>
+        /// <param name="context">
+        ///     The <see cref="DbContext"/> to be consumed by the service
+        /// </param>
+        /// <param name="mapper">
+        ///     The <see cref="IMapper"/> to be consumed by the service
+        /// </param>
+        /// 
         public BaseService(DbContext context, IMapper mapper)
         {
             Context = context;
             Mapper = mapper;
         }
 
+        /// <summary>
+        ///     Gives a way to set the <paramref name="context"/> property from subclasses.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         This constructor put a null in the Mapper property, so, you only must use 
+        ///         it if you will provide you own Mapper
+        ///     </para>
+        /// </remarks>
+        /// <param name="context">
+        ///     The <see cref="DbContext"/> to be consumed by the service
+        /// </param>
+        /// 
+        public BaseService(DbContext context)
+        {
+            Context = context;
+            Mapper = null;
+        }
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.QueryAll"/>.</para>
-        /// <para>Return a <em>"AsNoTracking"</em> list, to avoid problems with EF Core</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.QueryAll"/>.
         /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The utility of this is centralice <em>all</em> object-retriving code that get ALL objects in DB, in one 
+        ///         function (Not counting specific cases), acting like a <em>generic</em>.
+        ///     </para>
+        /// </remarks>
+        /// <returns>
+        ///     A readonly (Not tracked by the <see cref="DbContext"/>) <see cref="IQueryable{TEntity}"/>.
+        /// </returns>
+        /// 
         public IQueryable<TEntity> QueryAll()
             => Context
                 .Set<TEntity>()
                 .AsNoTracking();
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.QuerySingle(TKey)"/>.</para>
-        /// <para>Return a entity tracked by the context to fulfill with related entities.</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity}.QuerySingle(TKey)"/>.
         /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The utility of this is centralice <em>all</em> object-retriving code that get ONE object 
+        ///         in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.
+        ///     </para>
+        /// </remarks>
+        /// <returns>
+        ///     Return a <see cref="TEntity"/> founded by <paramref name="key"/> tracked by the context or null.
+        /// </returns>
+        /// <param name="key">
+        ///     The primary key to found the entity
+        /// </param>
+        /// 
         public async Task<TEntity> QuerySingle(TKey key)
             => await Context
                 .Set<TEntity>()
@@ -90,17 +181,32 @@ namespace Quicker.Abstracts.Service
 
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDTO(TEntity)"/>.</para>
-        /// <para>This function uses AutoMapper to work.</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDTO(TEntity)"/>
         /// </summary>
-        public TEntityDTO ToDTO(TEntity entity)
+        /// <remarks>
+        ///     <para>
+        ///         This function uses AutoMapper to work, but you can override it to use your own. To do this, 
+        ///         use the <see cref="BaseService{TKey, TEntity, TEntityDTO}.BaseService(DbContext)"/> constructor
+        ///     </para>
+        /// </remarks>
+        /// <param name="entity">The Domain entity to be mapped to DTO</param>
+        /// 
+        public virtual TEntityDTO ToDTO(TEntity entity)
             => Mapper.Map<TEntity, TEntityDTO>(entity);
 
+
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDomain(TEntityDTO)"/>.</para>
-        /// <para>This function uses AutoMapper to work.</para>
+        ///     Main implementation of <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDomain(TEntityDTO)"/>
         /// </summary>
-        public TEntity ToDomain(TEntityDTO entity)
+        /// <remarks>
+        ///     <para>
+        ///         This function uses AutoMapper to work, but you can override it to use your own. To do this, 
+        ///         use the <see cref="BaseService{TKey, TEntity, TEntityDTO}.BaseService(DbContext)"/> constructor
+        ///     </para>
+        /// </remarks>
+        /// <param name="entity">The DTO entity to be mapped to Domain</param>
+        /// 
+        public virtual TEntity ToDomain(TEntityDTO entity)
             => Mapper.Map<TEntityDTO, TEntity>(entity);
     }
 }
--- Quicker/Interfaces/Service/IBaseService.cs
+++ Quicker/Interfaces/Service/IBaseService.cs
@@ -9,59 +9,67 @@ using System.Threading.Tasks;
 namespace Quicker.Interfaces.Service
 {
     /// <summary>
-    /// <para>Interface to provide functions to get All or One Objects in DB, in classes who doesn't have a DTO related entity</para>
-    /// <para>The utility of this function is centralice all object-retriving code that get objects of DB in two functions.</para>
+    ///     Interface to provide functions to get All or One Objects in DB, in classes who doesn't have a DTO related entity
     /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The utility of this interface is centralice all object-retriving code that get objects of DB in two functions.    
+    ///     </para>
+    /// </remarks>
+    /// 
     public interface IBaseService<TKey, TEntity>
         where TEntity : class, IAbstractModel<TKey>
     {
         /// <summary>
-        /// <para>Method to provide the function to get ALL Objects in DB.</para>
-        /// <para>The utility of this is centralice <em>all</em> object-retriving code that get ALL objects in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.</para>
+        ///     Method to provide the function to get ALL Objects in DB.
         /// </summary>
+        /// 
         IQueryable<TEntity> QueryAll();
 
         /// <summary>
-        /// <para>Method to provide the function to get ONE Objects in DB.</para>
-        /// <para>The utility of this is centralice <em>all</em> object-retriving code that get ONE object in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.</para>
-        /// <para><paramref name="key"/>: Primary key value to find the element in DB </para>
+        ///     Method to provide the function to get ONE Objects in DB.
         /// </summary>
+        /// 
         Task<TEntity> QuerySingle(TKey key);
     }
 
     /// <summary>
-    /// <para>Interface to provide functions to get All or One Objects in DB, in classes who have a DTO related entity</para>
-    /// <para>The utility of this function is centralice all object-retriving code that get objects of DB in two functions.</para>
+    ///     Interface to provide functions to get All or One Objects in DB, in classes who have a DTO related entity.
     /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The utility of this interface is centralice all object-retriving code that get objects of DB in two functions.    
+    ///     </para>
+    /// </remarks>
+    /// 
     public interface IBaseService<TKey, TEntity, TEntityDTO>
         where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
         where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
     {
         /// <summary>
-        /// <para>Method to provide the function to get ALL Objects in DB.</para>
-        /// <para>The utility of this is centralice <em>all</em> object-retriving code that get ALL objects in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.</para>
+        ///     Method to provide the function to get ALL Objects in DB.
         /// </summary>
+        /// 
         IQueryable<TEntity> QueryAll();
 
         /// <summary>
-        /// <para>Method to provide the function to get ONE Objects in DB.</para>
-        /// <para>The utility of this is centralice <em>all</em> object-retriving code that get ONE object in DB, in one function (Not counting specific cases), acting like a <em>generic</em>.</para>
-        /// <para><paramref name="key"/>: Primary key value to find the element in DB </para>
+        ///     Method to provide the function to get ONE Objects in DB.
         /// </summary>
+        /// 
         Task<TEntity> QuerySingle(TKey key);
 
         /// <summary>
-        /// <para>Method to transform a class Domain instance to a DTO one.</para>
-        /// <para>Counterpart of: <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDomain(TEntityDTO)"/></para>
-        /// <para><paramref name="entity"/>: Instance to clone, and convert to related DTO </para>
+        ///     Method to transform a class Domain instance to a DTO one.
         /// </summary>
+        /// <param name="entity">Instance to clone, and convert to related DTO</param> 
+        /// 
         TEntityDTO ToDTO(TEntity entity);
 
         /// <summary>
-        /// <para>Method to transform a class DTO instance to a Domain one.</para>
-        /// <para>Counterpart of: <seealso cref="IBaseService{TKey, TEntity, TEntityDTO}.ToDTO(TEntity)"/></para>
-        /// <para><paramref name="entity"/>: Instance to clone, and convert to related Domain </para>
+        ///     Method to transform a class DTO instance to a Domain one.
         /// </summary>
+        /// <param name="entity">Instance to clone, and convert to related Domain</param> 
+        /// 
         TEntity ToDomain(TEntityDTO entity);
     }
 }
