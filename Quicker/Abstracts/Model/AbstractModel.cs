5824d27c62774b8f149cfb1ca2eb0eae941ddcd1
Hernan Alvarez <halvarez@Austranet.com>
2021-02-20 10:38:08 -0300

    Se modifico la documentacion de AbstractModel

--- Quicker/Abstracts/Model/AbstractModel.cs
+++ Quicker/Abstracts/Model/AbstractModel.cs
@@ -6,28 +6,18 @@ using System.Text;
 
 namespace Quicker.Abstracts.Model
 {
-    /// <summary>.
-    /// <para>Main implementation of <seealso cref="IAbstractModel{TKey}"/>.</para>
-    /// </summary> 
+    /// <summary>
+    ///     Implementacion principal de <see cref="IAbstractModel{TKey}"/>, que da valores predeterminados a los campos de dicha interface.
+    /// </summary>
+    /// <typeparam name="TKey">Representa el tipo de la PK en la base de datos. Suele ser <see cref="int"/> o <see cref="string"/></typeparam>
+    /// 
     public abstract class AbstractModel<TKey> : IAbstractModel<TKey>
     {
-        /// <summary>.
-        /// <para>Implementation of <seealso cref="IAbstractModel{TKey}.Id"/>.</para>
-        /// <para>Is Key and Required.</para>
-        /// </summary> 
         [Key, Required]
-        public TKey Id { get; set; }
+        public TKey Id { get; set; } = default;
 
-        /// <summary>.
-        /// <para>Implementation of <seealso cref="IAbstractModel{TKey}.CreatedAt"/>.</para>
-        /// <para>Is Required, and it default value is the <em>current date</em>.</para>
-        /// </summary> 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-        /// <summary>.
-        /// <para>Implementation of <seealso cref="IAbstractModel{TKey}.LastUpdated"/>.</para>
-        /// <para>Is Required, and it default value is the <em>current date</em>.</para>
-        /// </summary> 
         public DateTime LastUpdated { get; set; } = DateTime.Now;
     }
 }
