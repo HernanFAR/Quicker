9485972f7bed5aecc53992921a5204605aa45476
Hernan Alvarez <halvarez@Austranet.com>
2021-02-20 21:44:11 -0300

    Se agrego el servicio falso para los test

---
+++ Service.Test/FakeServices/FakeCloseService.cs
@@ -0,0 +1,14 @@
+﻿using Quicker.Abstracts.Service;
+using Quicker.Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeCloseService : CloseServiceAsync<int, TestModel>
+    {
+        public FakeCloseService(TestContext context) :
+            base(context) { }
+    }
+}
