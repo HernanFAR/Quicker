e8f1540a11ede8b3a58ac610085cf377d4553afe
Hernan Alvarez <halvarez@Austranet.com>
2021-02-21 08:12:44 -0300

    Se agrego el servicio de FakeCloseServiceDTO

---
+++ Service.Test/FakeServices/FakeCloseServiceDTO.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Quicker.Abstracts.Service;
+using Quicker.Test.Repository;
+using Quicker.Test.Repository.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeCloseServiceDTO : CloseServiceAsync<int, TestModelRelation, TestModelRelationDTO>
+    {
+        public FakeCloseServiceDTO(TestContext context, IMapper mapper) :
+            base(context, mapper) { }
+    }
+}
