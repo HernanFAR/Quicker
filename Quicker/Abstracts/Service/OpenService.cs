d9c2453df7221cf8fdb70db62d2662b6874ccd2a
Hernan Alvarez <halvarez@Austranet.com>
2021-02-22 11:02:44 -0300

    Se refactorizo OpenServiceAsync{TKey, TEntity, TEntityDTO}

--- Quicker/Abstracts/Service/OpenService.cs
+++ Quicker/Abstracts/Service/OpenService.cs
@@ -39,9 +39,9 @@ namespace Quicker.Abstracts.Service
 
         /// <summary>
         ///     Metodo para validar una entidad, antes de crearla, si no es valida, arroja un 
-        ///     <see cref="ValidationException"/>.
+        ///     <see cref="Fluent.ValidationException"/>.
         /// </summary>
-        /// <exception cref="ValidationException" />
+        /// <exception cref="Fluent.ValidationException" />
         /// <exception cref="ArgumentNullException" />
         /// 
         protected virtual void ValidateObjectBeforeCreating(TEntity entity)
@@ -161,17 +161,81 @@ namespace Quicker.Abstracts.Service
         public OpenServiceAsync(DbContext context, AutoMapper.IMapper mapper) : 
             base(context, mapper) { }
 
+        public OpenServiceAsync(DbContext context) :
+            base(context, null)
+        { }
+
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IOpenServiceAsync{TKey, TEntity}.Create(TEntity)"/>.</para>
+        ///     Metodo para validar una entidad, antes de crearla, si no es valida, arroja un 
+        ///     <see cref="Fluent.ValidationException"/>.
         /// </summary>
-        public async Task<TEntityDTO> Create(TEntityDTO entity)
+        /// <exception cref="Fluent.ValidationException" />
+        /// <exception cref="ArgumentNullException" />
+        /// 
+        protected virtual void ValidateObject(TEntityDTO entity)
         {
-            entity = ValidateObject(entity);
+            var context = new DA.ValidationContext(entity);
+            List<DA.ValidationResult> errors = new List<DA.ValidationResult>();
+
+            var isValid = DA.Validator.TryValidateObject(entity, context, errors, true);
+
+            if (isValid)
+                return;
+
+            List<ValidationFailure> validationFailures = new List<ValidationFailure>();
+
+            errors.ForEach(e =>
+                validationFailures.Add(
+                    new ValidationFailure(
+                        string.Join(", ", e.MemberNames),
+                        e.ErrorMessage
+                    )
+                )
+            );
+
+            throw new Fluent.ValidationException(validationFailures);
+        }
+
+        /// <summary>
+        ///     Metodo para presetear los valores de la entidad que se creara.
+        /// </summary>
+        /// 
+        protected virtual void PresetPropertiesBeforeCreating(TEntityDTO entity)
+        { }
+
+        /// <summary>
+        ///     <para>
+        ///         Crea un registro en la base de datos basado en la entidad ingresada por parametro, 
+        ///         si no es una entidad valida, arroja un <see cref="Fluent.ValidationException"/>.
+        ///     </para>
+        ///     <para>
+        ///         En caso de que haya algun problema al momento de actualizar en la base de datos,
+        ///         arroja un <see cref="DbUpdateConcurrencyException"/> o un <see cref="DbUpdateException"/>,
+        ///         si hay un conflicto con la ID, arroja un <see cref="InvalidOperationException"/>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <typeparamref name="TEntityDTO"/>
+        /// </returns>
+        /// <exception cref="Fluent.ValidationException" />
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="DbUpdateException" />
+        /// <exception cref="InvalidOperationException" />
+        /// <exception cref="DbUpdateConcurrencyException" />
+        /// <param name="entity">Entidad con la que se creara el registro</param>
+        /// 
+        public virtual async Task<TEntityDTO> Create(TEntityDTO entity)
+        {
+            ValidateObject(entity);
+
+            PresetPropertiesBeforeCreating(entity);
 
             entity.CreatedAt = DateTime.Now;
             entity.LastUpdated = DateTime.Now;
 
-            var tracked = Context.Set<TEntityDTO>().Add(entity);
+            var toCreate = ToDomain(entity);
+
+            var tracked = Context.Set<TEntity>().Add(toCreate);
             await Context.SaveChangesAsync();
 
             var created = await Query()
@@ -182,28 +246,40 @@ namespace Quicker.Abstracts.Service
 
             return dto;
         }
+        /// <summary>
+        ///     Filtro que se aplica para ver que elementos no borrar, en base a si tienen determinadas
+        ///     propiedades, si no lo pasa, arroja un <see cref="InvalidOperationException"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException" />
+        /// 
+        protected virtual void DeleteFilter(TEntityDTO entity) { }
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IOpenServiceAsync{TKey, TEntity}.Delete(TKey)"/>.</para>
+        ///     Borra el registro relacionado a la entidad que se paso por parametro, si pasa el 
+        ///     filtro, si no lo pasa, arroja un <see cref="InvalidOperationException"/>
         /// </summary>
-        public async Task Delete(TKey key)
+        /// <exception cref="InvalidOperationException" />
+        /// 
+        public virtual Task Delete(TEntityDTO entity)
+            => Delete(entity.Id);
+
+        /// <summary>
+        ///     Borra el registro relacionado a la PK que se paso por parametro, si pasa el filtro, 
+        ///     si no lo pasa, arroja un <see cref="InvalidOperationException"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException" />
+        /// 
+        public virtual async Task Delete(TKey key)
         {
             var entity = await Context.Set<TEntity>().FindAsync(key);
+
+            if (entity == null) throw new InvalidOperationException(nameof(entity));
+
+            DeleteFilter(entity);
 
             Context.Entry(entity).State = EntityState.Deleted;
 
             await Context.SaveChangesAsync();
-        }
-
-        /// <summary>
-        /// <para>Method for validate a entity before create it.</para>
-        /// <exception cref="ValidationException">If the provied entity is not valid and the validation form is the default (DataAnnotations).</exception>
-        /// </summary>
-        protected virtual TEntityDTO ValidateObject(TEntityDTO entity)
-        {
-            DA.Validator.ValidateObject(entity, new DA.ValidationContext(entity));
-
-            return entity;
         }
     }
 }
