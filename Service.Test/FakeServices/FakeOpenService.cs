98aab31983dd9b13f0b5e4730ccee029b1cd4ef7
Hernan Alvarez <halvarez@Austranet.com>
2021-02-21 22:13:49 -0300

    Se agrego el servicio falso para testing

---
+++ Service.Test/FakeServices/FakeOpenService.cs
@@ -0,0 +1,14 @@
+﻿using Quicker.Abstracts.Service;
+using Quicker.Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeOpenService : OpenServiceAsync<int, TestModel>
+    {
+        public FakeOpenService(TestContext context) :
+            base(context) { }
+    }
+}
