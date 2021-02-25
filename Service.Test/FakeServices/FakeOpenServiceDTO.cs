0e3ab336844cfc768c57b2194e7fa6f322e644ff
Hernan Alvarez <halvarez@Austranet.com>
2021-02-24 22:38:07 -0300

    Se agrego el servicio falso para tests de DTO de open service

---
+++ Service.Test/FakeServices/FakeOpenServiceDTO.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Quicker.Abstracts.Service;
+using Quicker.Test.Repository;
+using Quicker.Test.Repository.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeOpenServiceDTO : OpenServiceAsync<int, TestModelRelation, TestModelRelationDTO>
+    {
+        public FakeOpenServiceDTO(DbContext context) : 
+            base(context) { }
+
+        public FakeOpenServiceDTO(DbContext context, IMapper mapper) : 
+            base(context, mapper) { }
+    }
+}
