af0489bda91e9cab96e409a47bf8debb94efcb67
Hernan Alvarez <halvarez@Austranet.com>
2021-02-27 16:51:58 -0300

    Se completo la implementacion de FullServiceAsync, en su version con DTO

--- Quicker/Abstracts/Service/FullServiceAsync.cs
+++ Quicker/Abstracts/Service/FullServiceAsync.cs
@@ -3,7 +3,6 @@ using Quicker.Interfaces.Model;
 using Quicker.Interfaces.Service;
 using System;
 using System.Threading.Tasks;
-using AutoMapper;
 using DA = System.ComponentModel.DataAnnotations;
 using Fluent = FluentValidation;
 using System.Collections.Generic;
@@ -142,10 +141,36 @@ namespace Quicker.Abstracts.Service
             return entity;
         }
     }
-
     /// <summary>
-    /// <para>Main implementation of <seealso cref="IFullServiceAsync{TKey, TEntity}"/>.</para>
-    /// </summary>23
+    ///     Implementacion principal de <see cref="IFullServiceAsync{TKey, TEntity, TEntityDTO}"/>, que brinda 
+    ///     funciones de ayuda para la creacion, actualizacion y borrado de datos.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         Un <em>servicio completo</em> provee funciones de lectura, escritura, actualizacion 
+    ///         y borrado de elementos en la base de datos, dando el CRUD.
+    ///     </para>
+    /// </remarks>
+    /// <typeparam name="TKey">Tipo de la PK en la base de datos</typeparam>
+    /// <typeparam name="TEntity">
+    ///     <para>
+    ///         Tipo de la entidad que debe devolver la base de datos. 
+    ///     </para>
+    ///     <para>
+    ///         Debe ser una clase, implementar <see cref="IAbstractModel{TKey}"/> y <see cref="IDTOOf{TDomain}"/>, 
+    ///         con TDomain de valor <typeparamref name="TEntityDTO"/>
+    ///     </para>
+    /// </typeparam>
+    /// <typeparam name="TEntityDTO">
+    ///     <para>
+    ///         Tipo de la entidad que debe devolver el servicio. 
+    ///     </para>
+    ///     <para>
+    ///         Debe ser una clase, implementar <see cref="IAbstractModel{TKey}"/> y <see cref="IDomainOf{TDTO}"/>, 
+    ///         con TDTO de valor <typeparamref name="TEntity"/>
+    ///     </para>
+    /// </typeparam>
+    /// 
     public abstract class FullServiceAsync<TKey, TEntity, TEntityDTO> : OpenServiceAsync<TKey, TEntity, TEntityDTO>, IFullServiceAsync<TKey, TEntity, TEntityDTO>
         where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
         where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
@@ -155,39 +180,109 @@ namespace Quicker.Abstracts.Service
         { }
 
         /// <summary>
-        /// <para>Main implementation of <seealso cref="IFullService{TKey, TEntity}.Update(TKey, TEntity)"/>.</para>
-        /// <exception cref="InvalidOperationException">If the provied key and the entity key aren't equal.</exception>
-        /// <exception cref="ValidationException">If the provied entity is not valid and the validation form is the default (DataAnnotations).</exception>
+        ///     Metodo para validar una entidad, antes de actualizarla, si no es valida, arroja un 
+        ///     <see cref="Fluent.ValidationException"/>.
         /// </summary>
+        /// <remarks>
+        ///     Por defecto, aunque use <see cref="Fluent.ValidationException"/> para la excepcion, 
+        ///     usa DataAnnotations para funcionar.
+        /// </remarks>
+        /// <exception cref="Fluent.ValidationException" />
+        /// <exception cref="ArgumentNullException" />
+        /// 
+        protected virtual void ValidateObjectBeforeUpdating(TEntityDTO entity)
+        {
+            var context = new DA.ValidationContext(entity);
+            List<DA.ValidationResult> errors = new List<DA.ValidationResult>();
+
+            var isValid = DA.Validator.TryValidateObject(entity, context, errors, true);
+
+            if (isValid)
+                return;
+
+            List<ValidationFailure> validationFailures = new List<ValidationFailure>();
+
+            errors.ForEach(e =>
+                validationFailures.Add(
+                    new ValidationFailure(
+                        string.Join(", ", e.MemberNames),
+                        e.ErrorMessage
+                    )
+                )
+            );
+
+            throw new Fluent.ValidationException(validationFailures);
+        }
+
+        /// <summary>
+        ///     Metodo para presetear los valores de la entidad que se actualice.
+        /// </summary>
+        /// 
+        protected virtual void PresetPropertiesBeforeUpdating(TEntity updated, TEntity original)
+        { }
+
+        /// <summary>
+        ///     <para>
+        ///         Actualiza un registro en la base de datos basado en la entidad ingresada por parametro, 
+        ///         retornando la entidad una vez se actualice.
+        ///     </para>
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         Si se entrega una entidad nula, arroja un <see cref="ArgumentNullException"/>.
+        ///     </para>
+        ///     <para>
+        ///         Si la Id del modelo es diferente a la pasada por parametro, arroja un 
+        ///         <see cref="InvalidOperationException"/>, con mensaje <em>"key"</em>.
+        ///     </para>
+        ///     <para>
+        ///         Si la entidad no existe en la base de datos, retorna un null.
+        ///     </para>
+        ///     <para>
+        ///         Si la entidad no es valida, retorna un <see cref="Fluent.ValidationException"/>
+        ///     </para>
+        ///     <para>
+        ///         En caso de que haya algun problema al momento de actualizar en la base de datos,
+        ///         arroja un <see cref="DbUpdateConcurrencyException"/> o un <see cref="DbUpdateException"/>.
+        ///     </para>
+        /// </remarks>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <typeparamref name="TEntity"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="InvalidOperationException" />
+        /// <exception cref="Fluent.ValidationException" />
+        /// <exception cref="DbUpdateException" />
+        /// <exception cref="DbUpdateConcurrencyException" />
+        /// <param name="entity">Entidad con la que se creara el registro</param>
+        /// 
         public async Task<TEntityDTO> Update(TKey key, TEntityDTO entity)
         {
+            if (entity is null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
             if (!entity.Id.Equals(key))
-                throw new InvalidOperationException();
+                throw new InvalidOperationException(nameof(key));
 
             TEntity original = await Context.Set<TEntity>().FindAsync(key);
 
             if (original == null)
                 return null;
 
-            ValidateObjectBeforeCreating(entity);
-
             var domain = ToDomain(entity);
 
-            SetNonUpdatableFields(domain, original);
-            entity.LastUpdated = DateTime.Now;
+            ValidateObjectBeforeUpdating(entity);
+            PresetPropertiesBeforeUpdating(domain, original);
+
+            domain.CreatedAt = original.CreatedAt;
+            domain.LastUpdated = DateTime.Now;
 
             Context.Entry(entity).State = EntityState.Modified;
             await Context.SaveChangesAsync();
 
             return entity;
-        }
-
-        /// <summary>
-        /// <para>A method to set values that should not be updated through this method, but through other methods.</para>
-        /// </summary>
-        protected virtual void SetNonUpdatableFields(TEntity updated, TEntity original)
-        {
-            updated.CreatedAt = original.CreatedAt;
         }
     }
 }
