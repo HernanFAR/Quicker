89f86baf7b68a911fb307cdd12a69d81a76d5d91
Hernan Alvarez <halvarez@Austranet.com>
2021-02-27 19:22:57 -0300

    Se agregaron los servicios completos falsos para los testeos

---
+++ Service.Test/FakeServices/FakeFullService.cs
@@ -0,0 +1,15 @@
+﻿using Quicker.Abstracts.Service;
+using Quicker.Configuration;
+using Quicker.Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeFullServiceDTO : FullServiceAsync<int, TestModel>
+    {
+        public FakeFullServiceDTO(IServiceProvider service) : 
+            base(service) { }
+    }
+}
---
+++ Service.Test/FakeServices/FakeFullServiceDTO.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Quicker.Abstracts.Service;
+using Quicker.Configuration;
+using Quicker.Test.Repository;
+using Quicker.Test.Repository.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Test.FakeServices
+{
+    public class FakeFullServiceDTO : FullServiceAsync<int, TestModelRelation, TestModelRelationDTO>
+    {
+        public FakeFullServiceDTO(IServiceProvider service) : 
+            base(service) { }
+    }
+}
