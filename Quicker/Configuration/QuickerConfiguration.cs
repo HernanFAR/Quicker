a6bcfa0fd795b8a6bbde80f8be71a5f63165865c
Hernan Alvarez <halvarez@Austranet.com>
2021-02-27 10:15:34 -0300

    Se agregaron los archivos (Que ojala funcione) de configuracion

---
+++ Quicker/Configuration/QuickerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker.Configuration
+{
+    public class QuickerConfiguration
+    {
+        public bool UseAutoMapper { get; set; }
+
+        public bool UseLogger { get; set; }
+    }
+}
---
+++ Quicker/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Quicker.Configuration;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class QuickerConfigurationExtensions
+    {
+        public static void AddQuickerConfiguration(this IServiceCollection services, Action<QuickerConfiguration> customConfig) 
+        {
+            services.AddOptions<QuickerConfiguration>();
+
+            if (customConfig != null)
+                services.Configure(customConfig);
+        }
+        
+        public static void AddQuickerConfiguration(this IServiceCollection services) 
+            => AddQuickerConfiguration(null);
+    }
+}
