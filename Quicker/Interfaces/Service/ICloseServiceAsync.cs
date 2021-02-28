e8b0ad734595f2777d9b222a84246b06fbd6d2b9
Hernan Alvarez <halvarez@Austranet.com>
2021-02-28 10:30:43 -0300

    Se elimino estructuras using innecesarias

--- Quicker/Interfaces/Service/ICloseServiceAsync.cs
+++ Quicker/Interfaces/Service/ICloseServiceAsync.cs
@@ -1,7 +1,6 @@
 ﻿using Quicker.Interfaces.Model;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
