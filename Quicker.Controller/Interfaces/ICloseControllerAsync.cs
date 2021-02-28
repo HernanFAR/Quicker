d21596c8a70f8d656da7062e6d4bccc0cdd5dc80
Hernan Alvarez <halvarez@Austranet.com>
2021-02-28 10:02:52 -0300

    Se agrego la interface de ICloseControllerAsync

---
+++ Quicker.Controller/Interfaces/ICloseControllerAsync.cs
@@ -0,0 +1,150 @@
+﻿using Quicker.Interfaces.Model;
+using Quicker.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicker.Controller.Interfaces
+{
+    /// <summary>
+    ///     Interface para especificar un <em>controlador cerrado</em>, con entidades que no tienen un DTO relacionado.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         Un <em>controlador cerrado</em> consume un servicio cerrado, que tiene funciones de solo lectura de 
+    ///         elementos en la base de datos, dando la R de el CRUD.
+    ///     </para>
+    /// </remarks>
+    /// <typeparam name="TKey">Tipo de la PK en la base de datos</typeparam>
+    /// <typeparam name="TEntity">
+    ///     <para>
+    ///         Tipo de la entidad que debe devolver el servicio. 
+    ///     </para>
+    ///     <para>
+    ///         Debe ser una clase e implementar <see cref="IAbstractModel{TKey}"/>.
+    ///     </para>
+    /// </typeparam>
+    /// <typeparam name="TService">
+    ///     <para>
+    ///         Servicio que consume el controlador para obtener la informacion. 
+    ///     </para>
+    ///     <para>
+    ///         Debe ser una clase e implementar <see cref="ICloseServiceAsync{TKey, TEntity}"/>.
+    ///     </para>
+    /// </typeparam>
+    /// 
+    public interface ICloseControllerAsync<TKey, TEntity, TService>
+        where TEntity : class, IAbstractModel<TKey>
+        where TService : class, ICloseServiceAsync<TKey, TEntity>
+    {
+        /// <summary>
+        ///     Lee todos los elementos de la base de datos.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve un <see cref="IEnumerable{T}"/> con tipo <typeparamref name="TEntity"/>.
+        /// </returns>
+        /// 
+        Task<IEnumerable<TEntity>> Read();
+
+        /// <summary>
+        ///     Lee un elemento en la base de datos.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve la <typeparamref name="TEntity"/> relacionada con esa clave primaria.
+        /// </returns>
+        /// <param name="key">Valor de la Primary key a encontrar en la base de datos.</param>
+        /// 
+        Task<TEntity> Read(TKey key);
+
+        /// <summary>
+        ///     Lee elementos de la base de datos, de forma paginada.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve un <see cref="IEnumerable{T}"/> con tipo <typeparamref name="TEntity"/>, con un largo de maximo <paramref name="number"/>.
+        /// </returns>
+        /// <param name="number">Cantidad de elementos a tomar de la base de datos.</param>
+        /// <param name="page">Numero de pagina de los elementos</param>
+        /// 
+        Task<IEnumerable<TEntity>> Paginate(int number, int page);
+
+        /// <summary>
+        ///     Verifica la existencia de un recurso en la base ded atos, basandose en la PK
+        /// </summary>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <see cref="bool"/>.
+        /// </returns>
+        /// <param name="key">PK del elemento a encontrar</param>
+        /// 
+        Task<bool> CheckExistenceByKey(TKey key);
+
+        /// <summary>
+        ///     Verifica la existencia de un recurso en la base de datos, basandose en condiciones 
+        ///     establecidas
+        /// </summary>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <see cref="bool"/>.
+        /// </returns>
+        /// <param name="conditions">Condiciones para buscar el elemento</param>
+        /// 
+        Task<bool> CheckExistenceByConditions(params Expression<Func<TEntity, bool>>[] conditions);
+    }
+
+    public interface ICloseControllerAsync<TKey, TEntity, TEntityDTO, TService>
+        where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
+        where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
+        where TService : class, ICloseServiceAsync<TKey, TEntity, TEntityDTO>
+    {
+        /// <summary>
+        ///     Lee todos los elementos de la base de datos.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve un <see cref="IEnumerable{T}"/> con tipo <typeparamref name="TEntityDTO"/>.
+        /// </returns>
+        /// 
+        Task<IEnumerable<TEntityDTO>> Read();
+
+        /// <summary>
+        ///     Lee un elemento en la base de datos.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve la <typeparamref name="TEntityDTO"/> relacionada con esa clave primaria.
+        /// </returns>
+        /// <param name="key">Valor de la Primary key a encontrar en la base de datos.</param>
+        /// 
+        Task<TEntityDTO> Read(TKey key);
+
+        /// <summary>
+        ///     Lee elementos de la base de datos, de forma paginada.
+        /// </summary> 
+        /// <returns>
+        ///     Un <see cref="Task"/> que devuelve un <see cref="IEnumerable{T}"/> con tipo <typeparamref name="TEntityDTO"/>, con un largo de maximo <paramref name="number"/>.
+        /// </returns>
+        /// <param name="number">Cantidad de elementos a tomar de la base de datos.</param>
+        /// <param name="page">Numero de pagina de los elementos</param>
+        /// 
+        Task<IEnumerable<TEntityDTO>> Paginate(int number, int page);
+
+        /// <summary>
+        ///     Verifica la existencia de un recurso en la base ded atos, basandose en la PK
+        /// </summary>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <see cref="bool"/>.
+        /// </returns>
+        /// <param name="key">PK del elemento a encontrar</param>
+        /// 
+        Task<bool> CheckExistence(TKey key);
+
+        /// <summary>
+        ///     Verifica la existencia de un recurso en la base de datos, basandose en condiciones 
+        ///     establecidas
+        /// </summary>
+        /// <returns>
+        ///     Un <see cref="Task"/> que retorna un <see cref="bool"/>.
+        /// </returns>
+        /// <param name="conditions">Condiciones para buscar el elemento</param>
+        /// 
+        Task<bool> CheckExistenceByConditions(params Expression<Func<TEntity, bool>>[] conditions);
+    }
+}
