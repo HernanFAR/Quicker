7ccb201b06e924020981fdee15037bf1f4fc7c13
Hernan Alvarez <halvarez@Austranet.com>
2021-03-01 09:43:54 -0300

    Se agregaron los controlares para los test de Quiciker.Abstract.Controller

---
+++ XUnitTestProject1/Fake/FakeOpenControllerDTO.cs
@@ -0,0 +1,13 @@
+﻿using Quicker.Abstracts.Controller;
+using Quicker.Services.Test.Fake;
+using Test.Common.Repository;
+using Test.Common.Repository.DTO;
+
+namespace Quicker.Controller.Test.Fake
+{
+    public class FakeOpenControllerDTO : OpenControllerAsync<int, TestModelRelation, TestModelRelationDTO, FakeOpenServiceDTO>
+    {
+        public FakeOpenControllerDTO(FakeOpenServiceDTO service) : 
+            base(service) { }
+    }
+}
