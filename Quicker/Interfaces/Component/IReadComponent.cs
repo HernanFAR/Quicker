b527703afe8082703dd9eaefcde903621991bcce
Hernan Alvarez <halvarez@Austranet.com>
2021-04-05 10:10:50 -0400

    Se agrego la interface del component de lectura

--- Quicker/Interfaces/Component/IReadComponent.cs
+++ Quicker/Interfaces/Component/IReadComponent.cs
@@ -20,4 +20,19 @@ namespace Quicker.Interfaces.Component
 
         Task<bool> CheckExistence(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
     }
+
+    public interface IReadComponent<TKey, TEntity, TEntityDTO>
+        where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
+        where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
+    {
+        Task<IEnumerable<TEntityDTO>> Read(Func<Task<bool>> action = null);
+
+        Task<IEnumerable<TEntityDTO>> Paginate(int page, int number = 10, Func<Task<bool>> action = null);
+
+        Task<TEntityDTO> Read(TKey key, Func<Task<bool>> action = null);
+
+        Task<bool> CheckExistence(TKey key, Func<Task<bool>> action = null);
+
+        Task<bool> CheckExistence(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
+    }
 }
