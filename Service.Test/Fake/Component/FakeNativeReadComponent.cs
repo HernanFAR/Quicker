8cad1e8c8f16af5fe06dc3db4004f53999346e81
Hernan Alvarez <halvarez@Austranet.com>
2021-04-07 00:01:39 -0400

    Se agrego un componente falso nativo (Sin overrides) para testeo

---
+++ Service.Test/Fake/Component/FakeNativeReadComponent.cs
@@ -0,0 +1,14 @@
+﻿using Quicker.Abstracts.Component;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Common.Repository;
+
+namespace Quicker.Service.Test.Fake.Component
+{
+    public class FakeNativeReadComponent : ReadComponent<int, Category>
+    {
+        public FakeNativeReadComponent(IServiceProvider service) : 
+            base(service) { }
+    }
+}
