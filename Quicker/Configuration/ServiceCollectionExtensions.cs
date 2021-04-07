f2dc52cb78d655c166169041c396b0e850615f5c
Hernan Alvarez <halvarez@Austranet.com>
2021-04-07 00:09:05 -0400

    Se agregaron test de AddQuickerConfiguration y una advertencia para agregar los summaries de estas funciones

--- Quicker/Configuration/ServiceCollectionExtensions.cs
+++ Quicker/Configuration/ServiceCollectionExtensions.cs
@@ -3,17 +3,20 @@ using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    #warning Agregar summaries de estas funciones.
     public static class QuickerConfigurationExtensions
     {
-        public static void AddQuickerConfiguration(this IServiceCollection services, Action<QuickerConfiguration> customConfig) 
+        public static IServiceCollection AddQuickerConfiguration(this IServiceCollection services, Action<QuickerConfiguration> customConfig) 
         {
             services.AddOptions<QuickerConfiguration>();
 
             if (customConfig != null)
                 services.Configure(customConfig);
+
+            return services;
         }
         
-        public static void AddQuickerConfiguration(this IServiceCollection services) 
+        public static IServiceCollection AddQuickerConfiguration(this IServiceCollection services) 
             => services.AddQuickerConfiguration(null);
     }
 }
---
+++ Service.Test/Configuration/QuickerConfigurationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Quicker.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Quicker.Service.Test.Configuration
+{
+    public class QuickerConfigurationTests
+    {
+        [Fact]
+        public void AddQuickerConfiguration_DefaultConfig_Success()
+        {
+            var provider = new ServiceCollection()
+                .AddQuickerConfiguration()
+                .BuildServiceProvider();
+
+            var quickerConfiguration = provider.GetRequiredService<IOptions<QuickerConfiguration>>().Value;
+
+            Assert.True(quickerConfiguration.UseAutoMapper);
+            Assert.True(quickerConfiguration.UseLogger);
+        }
+
+        [Fact]
+        public void AddQuickerConfiguration_CustomConfig_Success()
+        {
+            var provider = new ServiceCollection()
+                .AddQuickerConfiguration(e => {
+                    e.UseLogger = false;
+                    e.UseAutoMapper = false;
+                })
+                .BuildServiceProvider();
+
+            var quickerConfiguration = provider.GetRequiredService<IOptions<QuickerConfiguration>>().Value;
+
+            Assert.False(quickerConfiguration.UseAutoMapper);
+            Assert.False(quickerConfiguration.UseLogger);
+        }
+    }
+}
