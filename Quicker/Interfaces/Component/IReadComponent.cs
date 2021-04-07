d81280dfac86a674f9d0404460f682c510ab150b
Hernan Alvarez <halvarez@Austranet.com>
2021-04-07 08:11:21 -0400

    Se cambio el nombre de los metodos, para mostrar cuales son asincronos

--- Quicker/Interfaces/Component/IReadComponent.cs
+++ Quicker/Interfaces/Component/IReadComponent.cs
@@ -10,29 +10,29 @@ namespace Quicker.Interfaces.Component
     public interface IReadComponent<TKey, TEntity>
         where TEntity : class, IAbstractModel<TKey>
     {
-        Task<IEnumerable<TEntity>> Read(Func<Task<bool>> action = null);
+        Task<IEnumerable<TEntity>> ReadAsync(Func<Task<bool>> action = null);
 
-        Task<IEnumerable<TEntity>> Paginate(int page, int number = 10, Func<Task<bool>> action = null);
+        Task<IEnumerable<TEntity>> PaginateAsync(int page, int number = 10, Func<Task<bool>> action = null);
 
-        Task<TEntity> Read(TKey key, Func<Task<bool>> action = null);
+        Task<TEntity> ReadAsync(TKey key, Func<Task<bool>> action = null);
 
-        Task<bool> CheckExistence(TKey key, Func<Task<bool>> action = null);
+        Task<bool> CheckExistenceAsync(TKey key, Func<Task<bool>> action = null);
 
-        Task<bool> CheckExistence(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
+        Task<bool> CheckExistenceAsync(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
     }
 
     public interface IReadComponent<TKey, TEntity, TEntityDTO>
         where TEntity : class, IAbstractModel<TKey>, IDomainOf<TEntityDTO>
         where TEntityDTO : class, IAbstractModel<TKey>, IDTOOf<TEntity>
     {
-        Task<IEnumerable<TEntityDTO>> Read(Func<Task<bool>> action = null);
+        Task<IEnumerable<TEntityDTO>> ReadAsync(Func<Task<bool>> action = null);
 
-        Task<IEnumerable<TEntityDTO>> Paginate(int page, int number = 10, Func<Task<bool>> action = null);
+        Task<IEnumerable<TEntityDTO>> PaginateAsync(int page, int number = 10, Func<Task<bool>> action = null);
 
-        Task<TEntityDTO> Read(TKey key, Func<Task<bool>> action = null);
+        Task<TEntityDTO> ReadAsync(TKey key, Func<Task<bool>> action = null);
 
-        Task<bool> CheckExistence(TKey key, Func<Task<bool>> action = null);
+        Task<bool> CheckExistenceAsync(TKey key, Func<Task<bool>> action = null);
 
-        Task<bool> CheckExistence(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
+        Task<bool> CheckExistenceAsync(Func<Task<bool>> action = null, params Expression<Func<TEntity, bool>>[] conditions);
     }
 }
